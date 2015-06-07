536dd9cb074f0f0b6963a89c6850e61fd1e60a8d
copy-ninja <jason@copyninja.net>
2015-06-06 21:01:22 -0500

    v4
    -updated stop behavior

--- MyQ.GarageDoorOpener.SmartDevice.groovy
+++ MyQ.GarageDoorOpener.SmartDevice.groovy
@@ -42,8 +42,6 @@ metadata {
 		capability "Sensor"
 		
 		attribute "lastActivity", "string"
-		
-		command "stop"
 	}
 
 	simulator {	}
@@ -53,10 +51,9 @@ metadata {
 			state("unknown", label:'${name}', action:"door control.close",  icon:"st.doors.garage.garage-open",    backgroundColor:"#ffa81e", nextState: "closing")
 			state("closed",  label:'${name}', action:"door control.open",   icon:"st.doors.garage.garage-closed",  backgroundColor:"#79b821", nextState: "opening")
 			state("open",    label:'${name}', action:"door control.close",  icon:"st.doors.garage.garage-open",    backgroundColor:"#ffa81e", nextState: "closing")
-			state("opening", label:'${name}', action:"stop",                icon:"st.doors.garage.garage-opening", backgroundColor:"#ffe71e", nextState: "stopped2")
-			state("closing", label:'${name}', action:"stop",                icon:"st.doors.garage.garage-closing", backgroundColor:"#ffe71e", nextState: "stopped1")
-			state("stopped1", label:'stopped', action:"door control.open",  icon:"st.doors.garage.garage-closing", backgroundColor:"#1ee3ff", nextState: "opening")
-			state("stopped2", label:'stopped', action:"door control.close", icon:"st.doors.garage.garage-opening", backgroundColor:"#1ee3ff", nextState: "closing")
+			state("opening", label:'${name}',		                icon:"st.doors.garage.garage-opening", backgroundColor:"#ffe71e", nextState: "open")
+			state("closing", label:'${name}',		                icon:"st.doors.garage.garage-closing", backgroundColor:"#ffe71e", nextState: "closed")
+			state("stopped", label:'stopped', action:"door control.close",  icon:"st.doors.garage.garage-opening", backgroundColor:"#1ee3ff", nextState: "closing")
 		}
 		standardTile("refresh", "device.door", inactiveLabel: false, decoration: "flat") {
 			state("default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh")
@@ -77,16 +74,6 @@ metadata {
 		details(["door", "lastActivity", "refresh"])
 	}
 }
-def installed() { initialize() } 
-def updated() { initialize() } 
-def initialize() { 
-	state.polling = [  
-		last: 0, 
-		runNow: true 
-	] 
-	refresh()  
-} 
-
 def parse(String description) {}
 
 def on() { 
@@ -99,78 +86,53 @@ def off() {
 
 def push() { 
 	def doorState = device.currentState("door")?.value
-	if (doorState == "open" || doorState == "stopped2") {
+	if (doorState == "open" || doorState == "stopped") {
 		close()
-	} else if (doorState == "closed" || doorState == "stopped1") {
+	} else if (doorState == "closed") {
 		open()
-	} else if (doorState == "opening"  || doorState == "closing" ) {
-		stop()
-	}
+	} 
 	sendEvent(name: "momentary", value: "pushed", display: false, displayed: false)
 }
 
 def open()  { 
 	parent.sendCommand(this, "desireddoorstate", 1) 
-	state.polling.runNow = true
 	updateDeviceStatus(4)
+	updateDeviceLastActivity(parent.getDeviceLastActivity(this))
 }
 def close() { 
 	parent.sendCommand(this, "desireddoorstate", 0) 
-	state.polling.runNow = true
 	updateDeviceStatus(5)
-}
-
-def stop() {
-	parent.sendCommand(this, "desireddoorstate", 3) 
-	state.polling.runNow = true
-	updateDeviceStatus(3)
+	updateDeviceLastActivity(parent.getDeviceLastActivity(this))
 }
 
 def refresh() {
-	state.polling.runNow = true
 	parent.refresh()
 	updateDeviceLastActivity(parent.getDeviceLastActivity(this))
 }
 
-def poll() {
-	updateDeviceStatus(parent.getDeviceStatus(this))
-	updateDeviceLastActivity(parent.getDeviceLastActivity(this))
-}
+def poll() { refresh() }
 
 // update status
 def updateDeviceStatus(status) {
-	//update the state data 
-	def next = (state.polling.last?:0) + 10000  //will not update if it's not more than 20 seconds. this is to stop polling from updating all the state data. 
-	if ((now() > next) || (state.polling.runNow)) { 
-		state.polling.last = now()
-		state.polling.runNow = false
-		
-		def currentState = device.currentState("door")?.value
-		if (status == "1" || status == "9") { 
-			sendEvent(name: "door", value: "open", display: true, descriptionText: device.displayName + " was open") 
-			sendEvent(name: "contact", value: "open", display: false, displayed: false)	
-		}   
-		if (status == "2") {
-			sendEvent(name: "door", value: "closed", display: true, descriptionText: device.displayName + " was closed")
-			sendEvent(name: "contact", value: "closed", display: false, displayed: false)
-		}
-		if (status == "3") { 
-			if (currentState == "opening" || currentState == "stopped2") {
-				sendEvent(name: "door", value: "stopped2", display: true, descriptionText: device.displayName + " was stopped") 
-			} else if (currentState == "closing" || currentState == "stopped1") {
-				sendEvent(name: "door", value: "stopped1", display: true, descriptionText: device.displayName + " was stopped") 
-			} else {
-				sendEvent(name: "door", value: "unknown", display: true, descriptionText: device.displayName + " was stopped") 
-			}
-		}
-		if (status == "4" || (status=="8" && currentState=="closed")) { 
-			sendEvent(name: "door", value: "opening", display: false, displayed: false) 
-		}  
-		if (status == "5" || (status=="8" && currentState=="open")) { 
-			sendEvent(name: "door", value: "closing", display: false, displayed: false) 
-		}  
+	def currentState = device.currentState("door")?.value
+	if (status == "1" || status == "9") { 
+		sendEvent(name: "door", value: "open", display: true, descriptionText: device.displayName + " is open") 
+		sendEvent(name: "contact", value: "open", display: false, displayed: false)	
+	}   
+	if (status == "2") {
+		sendEvent(name: "door", value: "closed", display: true, descriptionText: device.displayName + " is closed")
+		sendEvent(name: "contact", value: "closed", display: false, displayed: false)
 	}
-	updateDeviceLastActivity(parent.getDeviceLastActivity(this))
+	if (status == "3") { 
+		sendEvent(name: "door", value: "stopped", display: true, descriptionText: device.displayName + " has stopped")
+		sendEvent(name: "contact", value: "closed", display: false, displayed: false)
+	}
+	if (status == "4" || (status=="8" && currentState=="closed")) { 
+		sendEvent(name: "door", value: "opening", display: false, displayed: false) 
+	}  
+	if (status == "5" || (status=="8" && currentState=="open")) { 
+		sendEvent(name: "door", value: "closing", display: false, displayed: false) 
+	}  
 }
 
 def updateDeviceLastActivity(long lastActivity) {
