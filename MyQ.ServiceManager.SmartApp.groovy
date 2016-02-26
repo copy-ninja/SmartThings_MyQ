db49e8c95c3738237f02a7895385c0390f4ae88b
copy-ninja <jason@copyninja.net>
2016-02-26 10:10:28 -0600

    Update MyQ.ServiceManager.SmartApp.groovy

--- MyQ.ServiceManager.SmartApp.groovy
+++ MyQ.ServiceManager.SmartApp.groovy
@@ -12,7 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Last Updated : 7/15/2015
+ *  Last Updated : 11/30/2015
  *
  */
 definition(
