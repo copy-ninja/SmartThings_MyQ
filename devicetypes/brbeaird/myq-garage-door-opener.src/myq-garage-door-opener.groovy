73c073d20814f2e48712cddfc9577724ea2487d9
Brian Beaird <brbeaird@gmail.com>
2016-12-29 09:10:10 -0600

    Updated date

--- devicetypes/brbeaird/myq-garage-door-opener.src/myq-garage-door-opener.groovy
+++ devicetypes/brbeaird/myq-garage-door-opener.src/myq-garage-door-opener.groovy
@@ -12,7 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Last Updated : 12/9/2016
+ *  Last Updated : 12/29/2016
  *
  */
 metadata {
