f8722cad38d192cfd4f5846a7fb211db81397c37
Brian Beaird <brbeaird@gmail.com>
2017-01-12 21:18:45 -0600

    Fixed dates Fixed light copy/paste error

--- devicetypes/brbeaird/myq-garage-door-opener-nosensor.src/myq-garage-door-opener-nosensor.groovy
+++ devicetypes/brbeaird/myq-garage-door-opener-nosensor.src/myq-garage-door-opener-nosensor.groovy
@@ -1,7 +1,7 @@
 /**
  *  MyQ Garage Door Opener NoSensor
  *
- *  Copyright 2016 Brian Beaird
+ *  Copyright 2017 Brian Beaird
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -12,7 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Last Updated : 12/12/2016
+ *  Last Updated : 1/12/2017
  *
  */
 metadata {
--- devicetypes/brbeaird/myq-garage-door-opener.src/myq-garage-door-opener.groovy
+++ devicetypes/brbeaird/myq-garage-door-opener.src/myq-garage-door-opener.groovy
@@ -1,7 +1,7 @@
 /**
  *  MyQ Garage Door Opener
  *
- *  Copyright 2015 Jason Mok/Brian Beaird/Barry Burke
+ *  Copyright 2017 Jason Mok/Brian Beaird/Barry Burke
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
