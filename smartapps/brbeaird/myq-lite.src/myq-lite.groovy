c7227740405dd2a0025c23e84a9e05d8bc30c739
Brian Beaird <brbeaird@gmail.com>
2017-02-15 17:57:04 -0600

    Updated LastUpdated line

--- smartapps/brbeaird/myq-lite.src/myq-lite.groovy
+++ smartapps/brbeaird/myq-lite.src/myq-lite.groovy
@@ -12,7 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Last Updated : 1/12/2017
+ *  Last Updated : 2/13/2017
  *
  */
 definition(
