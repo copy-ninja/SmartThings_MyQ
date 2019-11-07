d84e9a5574de84ac9fd22be7ad7b299e77d4869b
Brian Beaird <brbeaird@gmail.com>
2019-11-07 14:12:59 -0600

    Default sensor battery to 100

--- devicetypes/brbeaird/myq-garage-door-opener.src/myq-garage-door-opener.groovy
+++ devicetypes/brbeaird/myq-garage-door-opener.src/myq-garage-door-opener.groovy
@@ -32,14 +32,14 @@ metadata {
 		capability "Sensor"
         capability "Battery"
         //capability "Health Check" Will be needed eventually for new app compatability but is not documented well enough yet
-		
+
 		attribute "lastActivity", "string"
         attribute "doorSensor", "string"
         attribute "doorMoving", "string"
         attribute "OpenButton", "string"
         attribute "CloseButton", "string"
         attribute "myQDeviceId", "string"
-        
+
 		command "updateDeviceStatus", ["string"]
 		command "updateDeviceLastActivity", ["number"]
         command "updateDeviceMoving", ["string"]
@@ -50,7 +50,7 @@ metadata {
 	simulator {	}
 
 	tiles {
-		
+
 		multiAttributeTile(name:"door", type: "lighting", width: 6, height: 4, canChangeIcon: false) {
 			tileAttribute ("device.door", key: "PRIMARY_CONTROL") {
 				attributeState "unknown", label:'${name}', icon:"st.doors.garage.garage-closed",    backgroundColor:"#ffa81e"
@@ -76,12 +76,12 @@ metadata {
 		standardTile("switch", "device.switch") {
 			state("on", label:'${name}', action: "switch.on",  backgroundColor:"#ffa81e")
 			state("off", label:'${name}', action: "switch.off", backgroundColor:"#79b821")
-		}		
+		}
         standardTile("openBtn", "device.OpenButton", width: 3, height: 3) {
             state "normal", label: 'Open', icon: "st.doors.garage.garage-open", backgroundColor: "#e86d13", action: "open", nextState: "opening"
             state "opening", label: 'Opening', icon: "st.doors.garage.garage-opening", backgroundColor: "#cec236", action: "open"
 		}
-        standardTile("closeBtn", "device.CloseButton", width: 3, height: 3) {            
+        standardTile("closeBtn", "device.CloseButton", width: 3, height: 3) {
             state "normal", label: 'Close', icon: "st.doors.garage.garage-closed", backgroundColor: "#00a0dc", action: "close", nextState: "closing"
             state "closing", label: 'Closing', icon: "st.doors.garage.garage-closing", backgroundColor: "#cec236", action: "close"
 		}
@@ -90,45 +90,45 @@ metadata {
 		}
 		valueTile("doorMoving", "device.doorMoving", width: 6, height: 2, inactiveLavel: false, decoration: "flat") {
 			state "default", label: '${currentValue}', backgroundColor:"#ffffff"
-		}        
+		}
         valueTile("battery", "device.battery", width: 2, height: 2, inactiveLavel: false, decoration: "flat") {
 			state "default", label: '${currentValue}% battery', backgroundColor:"#ffffff"
-		}        
+		}
         main "door"
 		details(["door", "openBtn", "closeBtn", "doorSensor", "battery", "refresh"])
 	}
 }
 
-def on() { 	
+def on() {
     log.debug "Turning door on!"
     open()
-    sendEvent(name: "switch", value: "on", isStateChange: true, display: true, displayed: true)	
+    sendEvent(name: "switch", value: "on", isStateChange: true, display: true, displayed: true)
 }
-def off() { 	
+def off() {
     log.debug "Turning door off!"
-    close()    
+    close()
 	sendEvent(name: "switch", value: "off", isStateChange: true, display: true, displayed: true)
 }
 
-def push() {	
+def push() {
     def doorState = device.currentState("door")?.value
 	if (doorState == "open" || doorState == "stopped") {
 		close()
 	} else if (doorState == "closed") {
 		open()
-	} 
+	}
 	sendEvent(name: "momentary", value: "pushed", display: false, displayed: false, isStateChange: true)
 }
 
-def open()  { 
+def open()  {
 	log.debug "Garage door open command called."
     parent.notify("Garage door open command called.")
     updateDeviceStatus("opening")
     parent.sendCommand(getMyQDeviceId(), "open")
-	
+
     runIn(20, refresh, [overwrite: true])	//Force a sync with tilt sensor after 20 seconds
 }
-def close() { 
+def close() {
 	log.debug "Garage door close command called."
     parent.notify("Garage door close command called.")
 	parent.sendCommand(getMyQDeviceId(), "close")
@@ -136,14 +136,14 @@ def close() {
     runIn(30, refresh, [overwrite: true]) //Force a sync with tilt sensor after 30 seconds
 }
 
-def getMyQDeviceId(){	    
+def getMyQDeviceId(){
     if (device.currentState("myQDeviceId")?.value)
     	return device.currentState("myQDeviceId").value
-	else{    	
-        def newId = device.deviceNetworkId.split("\\|")[2]        
+	else{
+        def newId = device.deviceNetworkId.split("\\|")[2]
         sendEvent(name: "myQDeviceId", value: newId, display: true , displayed: true)
         return newId
-    }	
+    }
 }
 
 def refresh() {
@@ -155,36 +155,36 @@ def refresh() {
 def poll() { refresh() }
 
 // update status
-def updateDeviceStatus(status) {	
-    
+def updateDeviceStatus(status) {
+
     def currentState = device.currentState("door")?.value
-    
+
     def switchState
     if (device.currentState("switch")?.value == "on"){switchState = "open"}
-    if (device.currentState("switch")?.value == "off"){switchState = "closed"}       
-    
-    
+    if (device.currentState("switch")?.value == "off"){switchState = "closed"}
+
+
     //Don't do anything if nothing changed
     if (currentState == status && switchState == status){
     	log.debug "No change; door is already set to " + status
         status = ""
     }
-    
+
     switch (status) {
 		case "open":
     		log.debug "Door is now open"
-			sendEvent(name: "door", value: "open", display: true, isStateChange: true, descriptionText: device.displayName + " is open") 
+			sendEvent(name: "door", value: "open", display: true, isStateChange: true, descriptionText: device.displayName + " is open")
 			sendEvent(name: "contact", value: "open", display: false, displayed: false, isStateChange: true)	// make sure we update the hidden states as well
-        	sendEvent(name: "switch", value: "on", display: false, displayed: false, isStateChange: true)		// on == open            
+        	sendEvent(name: "switch", value: "on", display: false, displayed: false, isStateChange: true)		// on == open
             break
-            
+
         case "closed":
 			log.debug "Door is now closed"
         	sendEvent(name: "door", value: "closed", display: true, isStateChange: true, descriptionText: device.displayName + " is closed")
 			sendEvent(name: "contact", value: "closed", display: false, displayed: false, isStateChange: true)	// update hidden states
-        	sendEvent(name: "switch", value: "off", display: false, displayed: false, isStateChange: true)		// off == closed            
+        	sendEvent(name: "switch", value: "off", display: false, displayed: false, isStateChange: true)		// off == closed
             break
-            
+
 		case "opening":
 			if (currentState == "open"){
         		log.debug "Door is already open. Leaving status alone."
@@ -202,14 +202,14 @@ def updateDeviceStatus(status) {
         		sendEvent(name: "door", value: "closing", display: false, displayed: false, isStateChange: true)
         	}
             break
-	
+
     	case "stopped":
     		if (currentState != "closed") {
     			log.debug "Door is stopped"
     			sendEvent(name: "door", value: "stopped", display: false, displayed: false, isStateChange: true)
         	}
             break
-            
+
         case "waiting":
         	if (currentState == "open") {
             	log.debug "Door is waiting before closing"
@@ -220,19 +220,23 @@ def updateDeviceStatus(status) {
 }
 
 def updateDeviceLastActivity(lastActivity) {
-	def finalString = lastActivity?.format('MM/d/yyyy hh:mm a',location.timeZone)    
+	def finalString = lastActivity?.format('MM/d/yyyy hh:mm a',location.timeZone)
 	sendEvent(name: "lastActivity", value: finalString, display: false , displayed: false)
 }
 
-def updateDeviceSensor(sensor) {	
+def updateDeviceSensor(sensor) {
 	sendEvent(name: "doorSensor", value: sensor, display: false , displayed: false)
 }
 
-def updateSensorBattery(batteryValue) {	
-	sendEvent(name: "battery", value: batteryValue, display: true, displayed: true)
+def updateSensorBattery(batteryValue) {
+	def newBattery = batteryValue
+    if (!batteryValue){
+    	newBattery = 100
+    }
+    sendEvent(name: "battery", value: newBattery, display: true, displayed: true)
 }
 
-def updateDeviceMoving(moving) {	
+def updateDeviceMoving(moving) {
 	sendEvent(name: "doorMoving", value: moving, display: false , displayed: false)
 }
 
